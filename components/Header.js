6a61e14926cc4f46bd6a2d7d0c2a7e644a4463ec
Christopher Buenaventura <christopherbuenaventura@gmail.com>
2023-03-13 15:43:10 -0700

    Build homepage with no icons

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <>
+      <div>cgbuen</div>
+      <div>Software</div>
+      <div>Concerts</div>
+      <div>Keyboards</div>
+      <div>About</div>
+      <div>Commissions</div>
+      <div>Resources</div>
+    </>
+  )
+}
