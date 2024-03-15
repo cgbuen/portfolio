e892d53fa6c030cab9ede6c8893a2392691ecaad
Christopher Buenaventura <christopherbuenaventura@gmail.com>
2024-03-15 11:48:24 -0700

    Update plates api usage
    - Use file extension from field data
    - Remove unnecessary cache buster

--- pages/api/plates.js
+++ pages/api/plates.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   const platesResponse = await fetch(`${ASSET_DOMAIN}/plates/plates.json?${Date.now()}`)
   const platesResponseJson = await platesResponse.json()
   const response = platesResponseJson.map(plate => {
-    plate.src = plate.src === 'n/a' ? plate.src : `${ASSET_DOMAIN}/plates/${plate.src}.svg?${plate.cache_buster}`
+    plate.src = plate.src === 'n/a' ? plate.src : `${ASSET_DOMAIN}/plates/${plate.src}`
     return plate
   })
   res.status(200).json(response)
